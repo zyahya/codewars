61db5647dcf3d61538568abcbc4fffdb570f3159
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-02 05:51:58 +0300

    Solve basic mathematical operation

---
+++ csharp/Codewars.Tests/Kyu8/BasicMathematicalOperationTests/BasicMathematicalOperationTests.cs
@@ -0,0 +1,13 @@
+﻿using Codewars.Solutions.Kyu8.BasicMathematicalOperation;
+
+namespace Codewars.Tests.Kyu8.BasicMathematicalOperationTests;
+
+public class BasicMathematicalOperationTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = BasicMathematicalOperation.Solution('-', 15, 18);
+        Assert.Equal(-3, result);
+    }
+}
