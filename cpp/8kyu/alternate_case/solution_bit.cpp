8f804b3da3820cfb3d1f21a3ae49845d6c581af8
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-05 15:29:07 +0300

    Add unit testing framework and proper tests for solved problems

--- cpp/8kyu/alternate_case/solution_bit.cpp
+++ cpp/8kyu/alternate_case/solution_bit.cpp
@@ -1,6 +1,9 @@
 #include <cassert>
 #include <string>
 
+namespace solution_bit
+{
+
 std::string solution(const std::string &str)
 {
     std::string newStr;
@@ -20,15 +23,4 @@ std::string solution(const std::string &str)
     return newStr;
 }
 
-int main()
-{
-    assert(solution("AbC") == "aBc");
-    assert(solution("HELLO WORLD") == "hello world");
-    assert(solution("1234") == "1234");
-    assert(solution("1a2b3c4d5e") == "1A2B3C4D5E");
-    assert(solution("String.prototype.toAlternatingCase") == "sTRING.PROTOTYPE.TOaLTERNATINGcASE");
-    assert(solution("altERnaTIng cAsE") == "ALTerNAtiNG CaSe");
-    assert(solution("altERnaTIng cAsE <=> ALTerNAtiNG CaSe") == "ALTerNAtiNG CaSe <=> altERnaTIng cAsE");
-
-    return 0;
-}
+} // namespace solution_bit
--- cpp/8kyu/alternate_case/solution_std.cpp
+++ cpp/8kyu/alternate_case/solution_std.cpp
@@ -2,6 +2,9 @@
 #include <cctype>
 #include <string>
 
+namespace solution_std
+{
+
 std::string solution(const std::string &str)
 {
     std::string newStr;
@@ -25,15 +28,4 @@ std::string solution(const std::string &str)
     return newStr;
 }
 
-int main()
-{
-    assert(solution("AbC") == "aBc");
-    assert(solution("HELLO WORLD") == "hello world");
-    assert(solution("1234") == "1234");
-    assert(solution("1a2b3c4d5e") == "1A2B3C4D5E");
-    assert(solution("String.prototype.toAlternatingCase") == "sTRING.PROTOTYPE.TOaLTERNATINGcASE");
-    assert(solution("altERnaTIng cAsE") == "ALTerNAtiNG CaSe");
-    assert(solution("altERnaTIng cAsE <=> ALTerNAtiNG CaSe") == "ALTerNAtiNG CaSe <=> altERnaTIng cAsE");
-
-    return 0;
-}
+} // namespace solution_std
